657c3b21b1c1c301c3fa6411474fe889709b31de
Keshav Maheshwari <keshav.maheshwari707@gmail.com>
2026-08-23 21:10:39 +0530

    made register & login pages

---
+++ frontend/src/app.routes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./features/auth/pages/Register";
+import Login from "./features/auth/pages/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+])
